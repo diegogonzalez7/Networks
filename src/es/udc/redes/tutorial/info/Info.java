1a168ad56e248a656a0110821377347c3d043df2
diegogonzalez7 <diego.gonzalez7@udc.es>
2023-03-15 15:07:37 +0100

    info

--- src/es/udc/redes/tutorial/info/Info.java
+++ src/es/udc/redes/tutorial/info/Info.java
@@ -1,9 +1,56 @@
 package es.udc.redes.tutorial.info;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.SimpleDateFormat;
+
 public class Info {
-
     public static void main(String[] args) {
-
+        String relativePath = args[0];
+        File file = new File(relativePath);
+        Path path = file.toPath();
+        try {
+            BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
+            System.out.println("Size: " + attr.size() + " bytes");
+            System.out.println("Last Modified: " + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(attr.lastModifiedTime().toMillis()));
+            System.out.println("Name: " + file.getName());
+            System.out.println("Extension: " + getFileExtension(file));
+            System.out.println("Type: " + getFileType(file));
+            System.out.println("Absolute Path: " + file.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
+
+    private static String getFileExtension(File file) {
+        String name = file.getName();
+        int index = name.lastIndexOf(".");
+        if (index == -1) {
+            return "";
+        }
+        return name.substring(index + 1);
+    }
+
+    private static String getFileType(File file) {
+        if (file.isDirectory()) {
+            return "Directory";
+        }
+        String extension = getFileExtension(file);
+        switch (extension) {
+            case "txt":
+            case "java":
+            case "py":
+                return "Text";
+            case "png":
+            case "jpg":
+            case "jpeg":
+            case "gif":
+                return "Image";
+            default:
+                return "Unknown";
+        }
+    }
 }
