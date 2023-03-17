fd0a66beb6322f3b25f1e0bd148e69830798f387
diegogonzalez7 <diego.gonzalez7@udc.es>
2023-03-17 02:33:13 +0100

    UDP

--- src/es/udc/redes/tutorial/udp/server/UdpServer.java
+++ src/es/udc/redes/tutorial/udp/server/UdpServer.java
@@ -7,34 +7,42 @@ import java.net.*;
  */
 public class UdpServer {
 
-    public static void main(String argv[]) {
+    public static void main(String [] argv) {
+        DatagramSocket datagramSocket = null;
+        DatagramPacket datagramPacket;
+        int puerto;
+
+
         if (argv.length != 1) {
             System.err.println("Format: es.udc.redes.tutorial.udp.server.UdpServer <port_number>");
             System.exit(-1);
         }
         try {
             // Create a server socket
-            
+
+            puerto = Integer.parseInt(argv[0]);
+            datagramSocket = new DatagramSocket(puerto);
+
             // Set maximum timeout to 300 secs
+            datagramSocket.setSoTimeout(300000);
             while (true) {
                 // Prepare datagram for reception
-                
+                datagramPacket = new DatagramPacket(new byte [20], 20);
                 // Receive the message
-                
+                datagramSocket.receive(datagramPacket);
                 // Prepare datagram to send response
-                
+                System.out.println("SERVER: Received «" + (new String(datagramPacket.getData())) + "» from "
+                        + datagramPacket.getAddress() + ":" + datagramPacket.getPort());
                 // Send response
-                
+                datagramSocket.send(datagramPacket);
             }
-          
-        // Uncomment next catch clause after implementing the logic
-        //} catch (SocketTimeoutException e) {
-        //    System.err.println("No requests received in 300 secs ");
+        } catch (SocketTimeoutException e) {
+            System.err.println("No requests received in 300 secs ");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
             e.printStackTrace();
         } finally {
-        // Close the socket
+            datagramSocket.close();
         }
     }
 }
