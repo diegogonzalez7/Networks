32f53fb521644b2a990c66e2ad5e044639df41d4
diegogonzalez7 <diego.gonzalez7@udc.es>
2023-03-17 19:09:28 +0100

    WebServer

--- src/es/udc/redes/webserver/ServerThread.java
+++ src/es/udc/redes/webserver/ServerThread.java
@@ -2,6 +2,15 @@ package es.udc.redes.webserver;
 
 import java.net.*;
 import java.io.*;
+import java.util.Date;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+
 
 
 public class ServerThread extends Thread {
@@ -14,17 +23,99 @@ public class ServerThread extends Thread {
     }
 
     public void run() {
+        String eco;
+        BufferedReader bufferedReader;
+        BufferedOutputStream bufferedOutputStream;
+        String fichero;
+        String tmp;
+        boolean error;
+        boolean date = true;
+
         try {
-          // This code processes HTTP requests and generates 
-          // HTTP responses
-          // Uncomment next catch clause after implementing the logic
-          //
-        // } catch (SocketTimeoutException e) {
-        //    System.err.println("Nothing received in 300 secs");
+            // Establece el canal de entrada y salida
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
+
+            // recibe el mensaje del cliente
+            eco = bufferedReader.readLine();
+            if(eco==null){
+                return;
+            }
+            error = !eco.split(" ")[0].equals("HEAD") && !eco.split(" ")[0].equals("GET");
+
+
+            fichero="p1-files/" + eco.split(" ")[1];
+
+            while((tmp=bufferedReader.readLine())!= null && !tmp.equals("")){
+                if(tmp.split(":")[0].equals("If-Modified-Since")){
+                    date=Date.from(Instant.from(DateTimeFormatter.RFC_1123_DATE_TIME.parse(tmp.split(": ",2)[1]))).compareTo(new Date(Files.getLastModifiedTime(Path.of(fichero)).toMillis()))>=0;
+                }
+            }
+
+            FileInputStream in = null;      //Get
+
+            if(Files.isDirectory(Path.of(fichero))) {
+                fichero = fichero + "/index.html";
+            }
+
+            if(error){
+                bufferedOutputStream.write("HTTP/1.0 400 Bad Request\r\n".getBytes());
+            }
+            else if(date) {
+                if (!Files.exists(Path.of(fichero))) {
+                    bufferedOutputStream.write("HTTP/1.0 404 Not Found\r\n".getBytes());
+                    fichero = "p1-files/error404.html";
+                } else {
+                    bufferedOutputStream.write("HTTP/1.0 200 OK\r\n".getBytes());
+                }
+            }
+            else{
+                bufferedOutputStream.write("HTTP/1.0 304 Not Modified\r\n".getBytes());
+            }
+            bufferedOutputStream.write(("Date: "+ OffsetDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME)+"\r\n").getBytes());
+            bufferedOutputStream.write("Server: WebServer_353\r\n".getBytes());
+
+            if(!error) {
+                bufferedOutputStream.write(("Last-Modified: " + DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneId.systemDefault()).format(Files.getLastModifiedTime(Path.of(fichero)).toInstant()) + "\r\n").getBytes());
+                bufferedOutputStream.write(("Content-Length: " + Files.size(Path.of(fichero)) + "\r\n").getBytes());
+                bufferedOutputStream.write(("Content-Type: " + Files.probeContentType(Path.of(fichero)) + "\r\n\r\n").getBytes());
+            } else {
+                bufferedOutputStream.write(("Last-Modified: " + OffsetDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME) + "\r\n").getBytes());
+                bufferedOutputStream.write("Content-Length: 0\r\n".getBytes());
+                bufferedOutputStream.write("Content-Type: text/plain\r\n\r\n".getBytes());
+            }
+
+
+            if(eco.split(" ")[0].equals("GET") && date && !error) {       //Head
+                try {
+                    in = new FileInputStream(fichero);
+                    int c;
+
+                    while ((c = in.read()) != -1) {
+                        bufferedOutputStream.write(c);
+                    }
+                } finally {
+                    if (in != null) {
+                        in.close();
+                    }
+                }
+            }
+            // Close the streams
+            bufferedOutputStream.close();
+            bufferedReader.close();
+
+         } catch (SocketTimeoutException e) {
+            System.err.println("Nothing received in 300 secs");
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
         } finally {
             // Close the client socket
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();        //muestra porqué ocurre el error
+            }
         }
     }
 }
--- src/es/udc/redes/webserver/WebServer.java
+++ src/es/udc/redes/webserver/WebServer.java
@@ -1,9 +1,46 @@
 package es.udc.redes.webserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 public class WebServer {
     
     public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket socket;
+        ServerThread fio;
 
+        if (args.length != 1) {
+            System.err.println("Format: es.udc.redes.webserver.WebServer <port>");
+            System.exit(-1);
+        }
+        try {
+            //Crea un ServerSocket
+            serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+            //Establece un temporizador de 300 segundos
+            serverSocket.setSoTimeout(300000);
+            while (true) {
+                //espera a las conexiones
+                socket = serverSocket.accept();
+                //crea un ServerThread, con el socket como parámetro
+                fio = new ServerThread(socket);
+                //Inicia el thread
+                fio.start();
+            }
+        } catch (SocketTimeoutException e) {
+            System.err.println("Nothing received in 300 secs");
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                assert serverSocket != null;
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
 }
